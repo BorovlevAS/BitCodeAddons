34a42d9cf02545f01fde176e00d15f1a94bab005
Borovlev A. S <bas@qdes.com.ua>
2023-03-06 00:23:18 +0200

    переводы, мелкая доработка форм

--- biko_fuel_excise/models/sale_order_line.py
+++ biko_fuel_excise/models/sale_order_line.py
@@ -10,6 +10,102 @@ class SaleOrderLine(models.Model):
     biko_density_15c = fields.Float(string="Density at 15 C")
     biko_product_qty_15c = fields.Float(string="Quantity at 15 C")
 
+    biko_qty_15c_deliv = fields.Float(
+        compute="_compute_biko_qty_15c_deliv",
+        string="15 C Delivered",
+        store=True,
+    )
+
+    biko_qty_15c_to_invoice = fields.Float(
+        compute="_get_to_invoice_qty_15c",
+        string="15 C To Inv",
+        store=True,
+        digits="Product Unit of Measure",
+    )
+
+    biko_qty_15c_invoiced = fields.Float(
+        compute="_compute_qty_15c_invoiced",
+        string="15 C Invoiced",
+        store=True,
+        digits="Product Unit of Measure",
+    )
+
+    @api.depends(
+        "invoice_lines.move_id.state",
+        "invoice_lines.biko_product_qty_15c",
+        "untaxed_amount_to_invoice",
+    )
+    def _compute_qty_15c_invoiced(self):
+
+        for line in self:
+            qty_invoiced = 0.0
+            for invoice_line in line._get_invoice_lines():
+                if (
+                    invoice_line.move_id.state != "cancel"
+                    or invoice_line.move_id.payment_state == "invoicing_legacy"
+                ):
+                    if invoice_line.move_id.move_type == "out_invoice":
+                        qty_invoiced += invoice_line.product_uom_id._compute_quantity(
+                            invoice_line.biko_product_qty_15c, line.product_uom
+                        )
+                    elif invoice_line.move_id.move_type == "out_refund":
+                        qty_invoiced -= invoice_line.product_uom_id._compute_quantity(
+                            invoice_line.biko_product_qty_15c, line.product_uom
+                        )
+            line.biko_qty_15c_invoiced = qty_invoiced
+
+    @api.depends(
+        "biko_qty_15c_invoiced",
+        "biko_qty_15c_deliv",
+        "product_uom_qty",
+        "order_id.state",
+    )
+    def _get_to_invoice_qty_15c(self):
+
+        for line in self:
+            if line.order_id.state in ["sale", "done"]:
+                if line.product_id.invoice_policy == "order":
+                    line.biko_qty_15c_to_invoice = (
+                        line.biko_product_qty_15c - line.biko_qty_15c_invoiced
+                    )
+                else:
+                    line.biko_qty_15c_to_invoice = (
+                        line.biko_qty_15c_deliv - line.biko_qty_15c_invoiced
+                    )
+            else:
+                line.biko_qty_15c_to_invoice = 0
+
+    @api.depends(
+        "move_ids.state",
+        "move_ids.scrapped",
+        "move_ids.product_uom_qty",
+        "move_ids.product_uom",
+        "move_ids.biko_product_qty_15c",
+    )
+    def _compute_biko_qty_15c_deliv(self):
+
+        for line in self:
+            if line.qty_delivered_method == "stock_move":
+                qty = 0.0
+                outgoing_moves, incoming_moves = line._get_outgoing_incoming_moves()
+                for move in outgoing_moves:
+                    if move.state != "done":
+                        continue
+                    qty += move.product_uom._compute_quantity(
+                        move.biko_product_qty_15c,
+                        line.product_uom,
+                        rounding_method="HALF-UP",
+                    )
+                for move in incoming_moves:
+                    if move.state != "done":
+                        continue
+                    qty -= move.product_uom._compute_quantity(
+                        move.biko_product_qty_15c,
+                        line.product_uom,
+                        rounding_method="HALF-UP",
+                    )
+                line.biko_qty_15c_deliv = qty
+
     def _prepare_procurement_values(self, group_id=False):
         """Prepare specific key for moves or other components that will be created from a stock rule
         comming from a sale order line. This method could be override in order to add other custom key that could
@@ -148,4 +244,17 @@ class SaleOrderLine(models.Model):
                 previos_qty_15=previous_product_uom_qty_15,
             )
 
+        return res
+
+    def _prepare_invoice_line(self, **optional_values):
+
+        self.ensure_one()
+        res = super(SaleOrderLine, self)._prepare_invoice_line(**optional_values)
+        res.update(
+            {
+                "biko_density_fact": self.biko_density_fact,
+                "biko_density_15c": self.biko_density_15c,
+                "biko_product_qty_15c": self.biko_qty_15c_to_invoice,
+            }
+        )
         return res
